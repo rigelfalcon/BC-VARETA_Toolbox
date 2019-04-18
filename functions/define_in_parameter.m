0c8f7d8fbd59012253a53f27f78a9c9f96773813
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2019-04-18 23:38:13 +0800

    Updating define parameters

--- functions/define_in_parameter.m
+++ functions/define_in_parameter.m
@@ -8,29 +8,28 @@ if(properties.run_mode ~= '1')
     disp('-----Waiting for Windows number and frequency''s resolution------');
     uiwait(guiHandle.UIFigure);
     if( isvalid(guiHandle) | ~guiHandle.canceled)
-        root_path = uigetdir('tittle','Select the Subject''s Folder');
-        if(root_path==0)
-            return;
-        end        
-        change_xml_parameter(strcat('properties',filesep,'properties.xml'),'properties','data_path',root_path);
-        [properties,folder] = get_properties(strcat('properties',filesep,'properties.xml'));
+        delete(guiHandle);        
     else
         fprintf(2,'-----------Canceled by User------------');
         delete(guiHandle);
         return
     end    
+    root_path = uigetdir('tittle','Select the Subject''s Folder');
+    if(root_path==0)
+        return;
+    end
+    change_xml_parameter(strcat('properties',filesep,'properties.xml'),'properties','data_path',root_path);    
     guiHandle = hhgm_params_guide;
     disp('-----Waiting for Windows number and frequency''s resolution------');
     uiwait(guiHandle.HHHGMParametersUIFigure);
     if( isvalid(guiHandle) | ~guiHandle.canceled)
-        delete(guiHandle);
-        [properties,folder] = get_properties(strcat('properties',filesep,'properties.xml'));
+        delete(guiHandle);      
     else
         fprintf(2,'-----------Canceled by User------------');
         delete(guiHandle);
         return
     end
-      
+     [properties,folder] = get_properties(strcat('properties',filesep,'properties.xml'));   
     %-----------------------------------------------------------------------
     %%
     
