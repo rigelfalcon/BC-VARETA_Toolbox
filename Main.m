27ea9fe71ff90132b8d95fd3a775bbc17cd3b1ed
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2019-05-05 13:19:04 +0800

    Creating a single starting point

--- Main.m
+++ Main.m
@@ -28,11 +28,14 @@ addpath('tools');
 
 %%  Test's Seccion
 %%----------------Start in the properties-----------------------------
-% --------   load the propoerties file to identify the move to Run (Guide or HPC)
-[properties,folder] = define_in_parameter();
-%% ----- Search the subject's folders and load the data's files
-Load_Files(folder,properties );
-%----------------End Load Subject's File-------------------------------
-%%
+file_path = strcat('properties',filesep,'properties.xml');
+strcat('properties',filesep,'properties.xml')
+root_tab =  'properties';
+parameter_name = 'run_mode';
+if (find_xml_parameter(file_path,root_tab,parameter_name,1)== '1')
+    BC_VARETA_bash;
+else
+    BC_VARETA_guide;
+end
 
 
