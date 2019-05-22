60624822e032adca1ad66a30627d920df68ab796
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2019-05-22 13:33:48 +0800

    Moving tools file

---
+++ tools/find_xml_parameter.m
@@ -0,0 +1,19 @@
+function result = find_xml_parameter(file_path,root_tab,parameter_name,value)
+%FIND_XML_PARAMETER Summary of this function goes here
+%   Detailed explanation goes here
+
+DOMnode = xmlread(file_path);
+bndbox_elem = DOMnode.getElementsByTagName(root_tab);
+element = bndbox_elem.item(0).getElementsByTagName(parameter_name);
+if(value)
+    try
+    result = char(element.item(0).getFirstChild.getData);
+    catch
+        result = [];
+    end
+else
+    result = element.item(0).getAttributes;
+end
+
+end
+
